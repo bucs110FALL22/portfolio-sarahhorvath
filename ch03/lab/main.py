d52f53ca56cb3ec2d94201134a53756a0f8d9f8a
shorvat1 <69004493+sarahhorvath@users.noreply.github.com>
2022-09-15 21:31:03 +0000

    transfer everything

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -18,6 +18,23 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
+#x = random.randrange (1,100)
+#print(x)
+#michelangelo.forward(x)
+#michelangelo.goto(-100,20)
+#y = random.randrange (1,100)
+#print(y)
+#leonardo.forward(y)
+#leonardo.goto(-100,-20)
+
+n = 10
+for _ in range(n):
+    x = random.randrange(0, 10)
+    michelangelo.forward(x)
+    michelangelo.goto(-100, 20)
+    y = random.randrange(0, 10)
+    leonardo.forward(7)
+    leonardo.goto(-100, -20)
 
 
 # PART B - complete part B here
