68208456a73d0fda884fc6e97f811f6a13949fa9
shorvat1 <69004493+sarahhorvath@users.noreply.github.com>
2022-09-16 15:57:23 +0000

    work on lab 3

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,5 +1,8 @@
 import turtle #1. import modules
 import random
+import pygame
+import math
+
 
 #Part A
 window = turtle.Screen() # 2.  Create a screen
@@ -11,6 +14,8 @@ michelangelo.color('orange')
 leonardo.color('blue')
 michelangelo.shape('turtle')
 leonardo.shape('turtle')
+michelangelo.speed(1)
+leonardo.speed(1)
 
 michelangelo.up() # 4. Pick up the pen so we don’t get lines
 leonardo.up()
@@ -18,26 +23,52 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
-#x = random.randrange (1,100)
-#print(x)
-#michelangelo.forward(x)
-#michelangelo.goto(-100,20)
-#y = random.randrange (1,100)
-#print(y)
-#leonardo.forward(y)
-#leonardo.goto(-100,-20)
+x = random.randrange (1,100)
+print(x)
+michelangelo.forward(x)
+y = random.randrange (1,100)
+print(y)
+leonardo.forward(y)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 n = 10
 for _ in range(n):
     x = random.randrange(0, 10)
     michelangelo.forward(x)
-    michelangelo.goto(-100, 20)
     y = random.randrange(0, 10)
     leonardo.forward(7)
-    leonardo.goto(-100, -20)
-
+    
+michelangelo.goto(-100, 20)
+leonardo.goto(-100, -20)
+window.exitonclick()
 
 # PART B - complete part B here
 
 
-window.exitonclick()
+pygame.init()
+screen = pygame.display.set_mode()
+screen.fill((255,255,255))
+color = (0,255,0)
+
+coords = []
+num_sides = 3
+side_length = 20
+offset = 100
+
+m = 3
+for _ in range(m):
+  theta = (2.0* math.pi * m) / (num_sides)
+  print (theta)
+  x2 = side_length * math.cos(theta) + offset  
+  y2 = side_length * math.sin(theta) + offset
+  print (x2,y2)
+
+pygame.draw.polygon(screen, color,((10,20),(90,20),(50,80)))
+pygame.display.flip()
+
+#equilateral triangle
+#square (4 sides)
+#hexacon (6 sides)
+#nonagon (9 sides)
+#Circle -ish (360 sides)
