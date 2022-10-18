4e412d2bbe115107ab9562a10f944fe56cd9ea55
shorvat1 <69004493+sarahhorvath@users.noreply.github.com>
2022-10-18 02:02:33 +0000

    work on boolean

---
+++ ch05/exercises/boolean_functions.py
@@ -0,0 +1,32 @@
+
+score = 100
+def percentage_to_letter(score):
+  if(score >= 90):
+    return "A"
+  elif(80 < score < 90):
+    return "B"
+  elif(70 < score < 80):
+    return "C"
+  elif(60 < score < 70):
+    return "D"
+  elif(score < 60):
+    return "F"
+
+
+def main():
+  result = percentage_to_letter(85);
+  print(result)
+
+main()
+# def is_passing(letter): 
+#   if (letter == "A"):
+#     return True
+#   elif (letter == "B"):
+#     return True  
+#   elif (letter == "C"):
+#     return True  
+#   elif (letter == "D"):
+#     return False  
+#   elif (letter == "F"):
+#     return False  
+
