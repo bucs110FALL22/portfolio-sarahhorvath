339287c649795bb2fa4f4dbb0a5e790723f99266
shorvat1 <69004493+sarahhorvath@users.noreply.github.com>
2022-10-27 19:23:01 +0000

    exam

--- ch06/exam/exam.py
+++ ch06/exam/exam.py
@@ -1,42 +1,39 @@
 #mood during the day 
 #if below a certain level - color changes from red to green depending on where you are in the grpah 
-# 
 
 import turtle 
-
-
+import random
 myTurtle = turtle.Turtle()
 myTurtle.shape("turtle")
 
 window = turtle.Screen ()
-turtle.setup(300,300)
+screenSize = 300
+turtle.setup(screenSize,screenSize)
 
-coordinates = [(-100, -300), (-100, 300), (-150,-300), (-150,0)]
-x=0
-for i in coordinates: 
-  myTurtle.hideturtle()
-  myTurtle.penup()
-  myTurtle.goto(coordinates[x])
-  x = x + 1
-  myTurtle.pendown()
-  myTurtle.goto(coordinates[x])
-  
-
-#def setup():
-
-  # make list with coordinates 
-#  coordinates 
- # x = (-100, -300)
- # y = (-100, 300)
-
-
-
+def setup():
+  coordinates = [(-100, -300), (-100, 300), (-150, 0), (150, 0)]
+  x=0
+  for i in coordinates: 
+    
+    myTurtle.hideturtle()
+    myTurtle.penup()
+    myTurtle.goto(coordinates[x])
+    x = x + 1
+    myTurtle.pendown()
+    myTurtle.goto(coordinates[x])
+    x = x + 1
+    myTurtle.penup()
+    if x+1 > len(coordinates):
+      return
+    if 
+    coordinates.append
+    
+setup()
 
 #myTurtle.goto(0,0)
 #myTurtle.speed(0)
-#setup()
 def foo(a, b, c, d):  
-    print(a,b,c,d)
+    print("Choose one:",a,b,c,d)
 
 foo("Great","Good","Meh", "Bad")
 
@@ -46,11 +43,27 @@ def mood():
   print(result)
   if result == "Great" or result == "great":
     print("yessss")
+    for i in range (0, screenSize):
+      myTurtle.goto(-100, 10)
+      myTurtle.pendown()
+      myTurtle.goto(-100,50)
+      myTurtle.penup()
   elif result == "Good" or result == "good":
     print("yessss")
   elif result == "Meh" or result == "meh":
     print("yessss")
   elif result == "Bad" or result == "bad":
     print("hi")
+#
+ 
+    
+
+
+
+
 mood()
+
+
+
+
 window.exitonclick()
