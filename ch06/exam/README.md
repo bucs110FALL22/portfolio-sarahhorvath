ba92efb7281893d317390a84c4c433477cb8ea59
shorvat1 <69004493+sarahhorvath@users.noreply.github.com>
2022-10-28 19:09:14 +0000

    finished exam

--- ch06/exam/README.md
+++ ch06/exam/README.md
@@ -1,9 +1,10 @@
 # CS110 Midterm Exam
 
 ## SHORT DESCRIPTION *(1 or 2 sentences describing your program)*
+My program uses turtle, functions, and a list to draw a lined piece of a paper. 
 
 ## KNOWN BUGS AND INCOMPLETE PARTS *(list any known bugs or non-working parts)*
-
+My project is complete. 
 ## REFERENCES *(any resources you use outside of class materials)*
 I used the following website to learn about how to append multiple values to the list. In my code, I use coordinates.extend([])
 https://reactgo.com/python-append-multiple-values-to-list/ 
--- ch06/exam/main.py
+++ ch06/exam/main.py
@@ -1,46 +1,43 @@
 import turtle 
-#import random
-myTurtle = turtle.Turtle()
-myTurtle.shape("turtle")
-
 window = turtle.Screen ()
-screenSize = 300
-turtle.setup(screenSize,screenSize)
-myTurtle.hideturtle()
-
-def main():
-    xCoordinate = -150
-    coordinates = [xCoordinate, -screenSize, xCoordinate, screenSize]
-                   #, -screenSize/2, yCoordinate, screenSize/2, yCoordinate]
-    x=0
-    for i in coordinates: 
-      myTurtle.penup()
-      y = x + 1
-      myTurtle.goto((coordinates[x],coordinates[y]))
-      myTurtle.pendown()
-      x = x + 2
-      y = y+2
-      myTurtle.goto((coordinates[x],coordinates[y]))
-      x = x+2
-      y = y+2
-      xCoordinate = xCoordinate +20
-      coordinates.extend([xCoordinate, -screenSize, xCoordinate, screenSize])
-      if xCoordinate>screenSize/2:
-        return
-      if x+1 > len(coordinates):
-        return
-      
-main()
+myTurtle = turtle.Turtle()
+SCREEN_SIZE = 300
 
 def coloring(bgcolor,fillcolor): 
   window.bgcolor(bgcolor)
   turtle.fillcolor(fillcolor)
-coloring("light blue", "green")
 
 def writing(message):
   turtle.write(message, move=False, align='left', font=('Arial', 8, 'normal'))
-writing("This is my lined paper!")
+
+def get_spacing(SCREEN_SIZE, number_of_lines):
+  return SCREEN_SIZE/number_of_lines
+def drawing_lines(yCoordinate):
+    coordinates = [-SCREEN_SIZE/2, yCoordinate, SCREEN_SIZE/2, yCoordinate]
+    x_value_in_coordinates=0
+    space = get_spacing(SCREEN_SIZE,10)
+    for i in coordinates: 
+      myTurtle.penup()
+      for i in (0,SCREEN_SIZE/2):
+        y_value_in_coordinates = x_value_in_coordinates + 1
+        myTurtle.goto((coordinates[x_value_in_coordinates],coordinates[y_value_in_coordinates]))
+        myTurtle.pendown()
+        x_value_in_coordinates = x_value_in_coordinates + 2
+        y_value_in_coordinates = y_value_in_coordinates+2
+      yCoordinate = yCoordinate +space
+      coordinates.extend([-SCREEN_SIZE/2, yCoordinate, SCREEN_SIZE/2, yCoordinate])
+      if yCoordinate>SCREEN_SIZE/2:
+        break
+      if x_value_in_coordinates+1 > len(coordinates):
+        break
+def main():
+  turtle.setup(SCREEN_SIZE,SCREEN_SIZE)
+  myTurtle.shape("turtle")
+  myTurtle.hideturtle()
+  drawing_lines(-150)
+  coloring("light blue", "green")
+  writing("This is my lined paper!")
+main()
 
 
 window.exitonclick()
-
