c7ea2d5e013563032fe4c1e608e4ac27962f7a9a
shorvat1 <69004493+sarahhorvath@users.noreply.github.com>
2022-11-11 16:55:17 +0000

    fixed lab 7

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -1,18 +1,20 @@
 class Rectangle:
     def __init__(self, x, y, h, w):
-        self.x = x
-        self.y = y
-        self.h = h
-        self.w = w
-        if self.x < 0: 
-          self.x = 0
-        elif self.y <0: 
-          self.y = 0 
-        elif self.h <0: 
-          self.h = 0 
-        elif self.w <0: 
-          self.w = 0 
+      '''takes x and y coordinates, height, width, and saves them as instance variables'''
+      self.x = x
+      self.y = y
+      self.h = h
+      self.w = w
+      if self.x < 0: 
+        self.x = 0
+      elif self.y <0: 
+        self.y = 0 
+      elif self.h <0: 
+        self.h = 0 
+      elif self.w <0: 
+        self.w = 0 
 
     def __str__(self):
-        result_str = f"x:{self.x}, y:{self.y}, h:{self.h}, w:{self.w}"
-        return result_str
+      '''returns a string containing the x, y, width, and height of rectangle'''
+      result_str = f"x:{self.x}, y:{self.y}, h:{self.h}, w:{self.w}"
+      return result_str
--- ch07/lab/Surface.py
+++ ch07/lab/Surface.py
@@ -3,9 +3,10 @@ from Rectangle import Rectangle
 class Surface:
   
   def __init__(self, filename, x, y, h, w):
+    '''filename string saves to instance variables and creates rectangle objects'''
     self.rect = Rectangle(x,y,w,h)
     self.image = filename 
     
   def getRect(self):
-    
+    '''returns the rectangle object'''
     return self.rect
